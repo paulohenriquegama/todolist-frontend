ee3d06c8a2b9fd81e302e3b0873aad5547ede1b2
PAULO HENRIQUE GAMA <paulohg_pool@hotmail.com>
2021-09-15 17:04:47 -0300

    update Api

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const Api = axios.create({
-  baseURL: 'https://mytodolist-frontend.herokuapp.com/',
+  baseURL: 'https://mytodolist-frontend.herokuapp.com',
 })
 
 export default Api;
